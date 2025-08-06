25006a9b1e91e6021d686f5787491cebc3122785
Gavin Parra <gavinparra@ymail.com>
2025-08-06 10:59:29 -0600

    reorganized some content

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './Home.css'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import TwoThreeSevenTen from "./pages/TwoThreeSevenTen.jsx"
+import './styles.css'
+import { Layout } from './Layout.jsx'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="/two-three-seven-ten" element={<TwoThreeSevenTen />} /> 
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
 
 createRoot(document.getElementById('root')).render(
-  <Router>
+  
     <StrictMode>
       <App />
     </StrictMode>
-  </Router>
+  
 )
