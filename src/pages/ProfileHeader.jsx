94d82e2f117d6bf50c952ee73b721830ea12f405
Alec <alecv1528@gmail.com>
2025-08-08 07:14:18 -0700

    started code for popup menu on ProfileHeader.jsx

--- src/pages/ProfileHeader.jsx
+++ src/pages/ProfileHeader.jsx
@@ -1,13 +1,27 @@
+import { useState} from "react";
 
 export default function ProfileHeader(){
 
     const userName = JSON.parse(localStorage.getItem('userName'))
     const userPicture = JSON.parse(localStorage.getItem('userPicture'))
 
+    const [ showProfileMenu, setShowProfileMenu ] = useState(false);
+
     return (
         <div className="flex justify-end items-center bg-transparent font-bold text-gray-500 gap-4">
             <span>Waddup {userName || "Playa"} </span>
-            <img src={userPicture} alt="profile-picture" className="inline w-20 h-20 rounded-full object-cover border-2 border-transparent"/>
+
+
+            <img onClick-={() => setShowProfileMenu(true)} src={userPicture} alt="profile-picture" className="inline w-20 h-20 rounded-full object-cover border-2 border-transparent hover: cursor-pointer" />
+            {showProfileMenu && (
+            <div>
+                <ul>
+                    <li>option 1</li>
+                    <li>option 2</li>
+                    <li>option 3</li>
+                </ul>
+            </div>
+        )}
         </div>
     )
 }
