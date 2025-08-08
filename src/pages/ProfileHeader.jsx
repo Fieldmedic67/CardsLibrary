60d536cb62beed82d1905cd20027753177b0babd
Alec <alecv1528@gmail.com>
2025-08-08 12:56:36 -0700

    Enhanced `ProfileHeader` menu and functionality
    - Added hover state to menu items in `ProfileHeader.css`.
    - Introduced state handling for toggling the username form.
    - Updated "Change Username" option with button and basic form logic.

--- src/pages/ProfileHeader.jsx
+++ src/pages/ProfileHeader.jsx
@@ -8,6 +8,7 @@ export default function ProfileHeader(){
     const userPicture = JSON.parse(localStorage.getItem('userPicture'))
 
     const [ showProfileMenu, setShowProfileMenu ] = useState(false);
+    const [ showUserNameForm, setShowUserNameForm ] = useState(false);
 
     return (
         <div className="flex justify-end items-center bg-transparent font-bold text-gray-500 gap-4">
@@ -18,21 +19,24 @@ export default function ProfileHeader(){
             {showProfileMenu && (
             <div className='hidden-menu-container'>
                 <ul className="option-list">
-                    <li className="hidden-menu-option" key="change-username">Change Username</li>
+                    <li className="hidden-menu-option" key="change-username"><button onClick={(event) => console.log(event)}>Change Username</button></li>
                     <li className="hidden-menu-option" key="change-profile-picture">Change Profile Picture</li>
                 </ul>
             </div>
         )}
-            <div className="hidden-change-username" hidden>
-                <form onSubmit={(event) => console.log(event)}>
-                    <label>
-                        Change Username
-                        <input type="text"/>
-                    </label>
-                    <button type="submit">Submit</button>
-                </form>
 
-            </div>
+            {showUserNameForm && (
+                <div className="hidden-change-username">
+                    <form onSubmit={(event) => console.log(event)}>
+                        <label>
+                            Change Username
+                            <input type="text"/>
+                        </label>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )}
+
         </div>
     )
 }
