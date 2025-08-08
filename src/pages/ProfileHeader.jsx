56e0d6074c774fe5bd8a0e7f262ccd4df6606101
Alec <alecv1528@gmail.com>
2025-08-08 12:42:59 -0700

    Updated `ProfileHeader` styles and enhanced menu functionality
    - Adjusted dimensions and background color in `ProfileHeader.css`.
    - Added `.hidden-menu-option` styles for new menu items.
    - Enhanced hidden menu with "Change Username" and "Change Profile Picture" options.
    - Integrated a basic form for username change.

--- src/pages/ProfileHeader.jsx
+++ src/pages/ProfileHeader.jsx
@@ -18,12 +18,21 @@ export default function ProfileHeader(){
             {showProfileMenu && (
             <div className='hidden-menu-container'>
                 <ul className="option-list">
-                    <li>option 1</li>
-                    <li>option 2</li>
-                    <li>option 3</li>
+                    <li className="hidden-menu-option" key="change-username">Change Username</li>
+                    <li className="hidden-menu-option" key="change-profile-picture">Change Profile Picture</li>
                 </ul>
             </div>
         )}
+            <div className="hidden-change-username" hidden>
+                <form onSubmit={(event) => console.log(event)}>
+                    <label>
+                        Change Username
+                        <input type="text"/>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+
+            </div>
         </div>
     )
 }
